fa54a04bbc2e39cbf83096ffa33c82fd18d888c3
Andres Gimeno <andresgimeno_20@hotmail.com>
2023-01-05 18:01:18 -0300

    DB Update, STR print funtion for models

--- mvt_coderhouse_project/users_mgmt_app/models.py
+++ mvt_coderhouse_project/users_mgmt_app/models.py
@@ -9,3 +9,6 @@ class Member(models.Model):
     email = models.EmailField(max_length=20,null=True)
     preferr_number = models.IntegerField(null=True)
     preferr_color = models.CharField(max_length=10,null=True)
+
+    def __str__(self) -> str:
+        return f"{self.first_name}, {self.last_name}"
