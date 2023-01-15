5e915883b0c1118db359af15e2a93af8557a805b
Andres Gimeno <andresgimeno_20@hotmail.com>
2023-01-15 16:29:47 -0300

    Added Readme File

--- mvt_coderhouse_project/users_mgmt_app/admin.py
+++ mvt_coderhouse_project/users_mgmt_app/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from users_mgmt_app.models import Member, Client, Group, User
+from users_mgmt_app.models import Client, Group, User
 
-admin.site.register(Member)
 admin.site.register(Client)
 admin.site.register(Group)
 admin.site.register(User)
